fcc98dc685c4c3e5ebf3fbd4e84d5f6dde739447
Cameron Hedgecock <cameronhedge@hotmail.co.uk>
2021-06-18 22:09:57 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,231 @@
+# 4:35:08
+from kivy.config import Config
+
+Config.set('graphics', 'width', 900)
+Config.set('graphics', 'height', 400)
+
+from random import randint
+from kivy import platform
+from kivy.core.window import Window
+from kivy.app import App
+from kivy.graphics import Color, Line, Quad, Triangle
+from kivy.properties import NumericProperty, Clock
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    from transforms import transform, transform_2D, transform_perspective
+    from user_actions import on_keyboard_up, on_keyboard_down, on_touch_down, on_touch_up, keyboard_closed
+    perspective_point_x = NumericProperty(0)
+    perspective_point_y = NumericProperty(0)
+
+    V_NB_LINES = 8
+    V_LINES_SPACING = .4
+    vertical_lines = []
+
+    H_NB_LINES = 15
+    H_LINES_SPACING = .1
+    horizontal_lines = []
+
+    SPEED = .8
+    current_offset_y = 0
+    current_y_loop = 0
+
+    SPEED_X = 20
+    current_speed_x = 0
+    current_offset_x = 0
+
+    NB_TILES = 16
+    tiles = []
+    tiles_coordinates = []
+
+    SHIP_WIDTH = .1
+    SHIP_HEIGHT = 0.035
+    SHIP_BASE_Y = 0.04
+    ship = None
+
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+        self.init_horizontal_lines()
+        self.init_vertical_lines()
+        self.init_tiles()
+        self.init_ship()
+        self.pre_fill_tiles_coordinates()
+        self.generate_tiles_coordinates()
+        if self.is_desktop():
+            self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
+            self._keyboard.bind(on_key_down=self.on_keyboard_down)
+            self._keyboard.bind(on_key_up=self.on_keyboard_up)
+        Clock.schedule_interval(self.update, 1/60)
+
+    def init_ship(self):
+        with self.canvas:
+            Color(0, 0, 0)
+            self.ship = Triangle()
+
+    def update_ship(self):
+        center_x =self.width / 2
+        base_y = self.SHIP_BASE_Y * self.height
+        ship_half_width = self.SHIP_WIDTH * self.width / 2
+        ship_height = self.SHIP_HEIGHT * self.height
+
+        x1, y1 = self.transform(center_x - ship_half_width, base_y)
+        x2, y2 = self.transform(center_x, base_y + ship_height)
+        x3, y3 = self.transform(center_x + ship_half_width, base_y)
+
+        self.ship.points = [x1, y1, x2, y2, x3, y3]
+
+
+    def is_desktop(self):
+        print(platform)
+        if platform in ('linux', 'win', 'macosx'):
+            return True
+        return False
+
+    def on_parent(self, widget, parent):
+        pass
+
+    def on_size(self, *args):
+        self.perspective_point_x = self.width/2
+        self.perspective_point_y = self.height * 0.75
+
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
+
+    def init_horizontal_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line())
+
+    def init_tiles(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.NB_TILES):
+                self.tiles.append(Quad())
+
+    def pre_fill_tiles_coordinates(self):
+        for i in range(10):
+            self.tiles_coordinates.append((0, i))
+
+    def generate_tiles_coordinates(self):
+        min_index = -int(self.V_NB_LINES / 2) + 1
+        max_index = min_index + self.V_NB_LINES - 1
+
+        last_x = 0
+        last_y = 0
+
+        for i in range(len(self.tiles_coordinates) - 1, -1, -1):
+            if self.tiles_coordinates[i][1] < self.current_y_loop:
+                del self.tiles_coordinates[i]
+        if len(self.tiles_coordinates) > 0:
+            last_coordinates = self.tiles_coordinates[-1]
+            last_x = last_coordinates[0]
+            last_y = last_coordinates[1] + 1
+
+
+        for i in range(len(self.tiles_coordinates), self.NB_TILES):
+            r = randint(0, 2)
+            # 0 Straight
+            # 1 Right
+            # 2 Left
+            self.tiles_coordinates.append((last_x, last_y))
+            if r == 1:
+                if last_x == max_index - 1:
+                    last_x -= 1
+                else:
+                    last_x += 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+            if r == 2:
+                if last_x == min_index + 1:
+                    last_x += 1
+                else:
+                    last_x -= 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+
+            last_y += 1
+
+
+    def get_line_x_from_index(self, index):
+        central_line_x = self.perspective_point_x
+        spacing = self.V_LINES_SPACING * self.width
+        offset = index - 0.5
+        line_x = central_line_x + offset * spacing + self.current_offset_x
+        return line_x
+
+    def get_line_y_from_index(self, index):
+        spacing = self.H_LINES_SPACING * self.height
+        line_y = index * spacing - self.current_offset_y
+        return line_y
+
+    def get_tile_coordinates(self, ti_x, ti_y):
+        ti_y = ti_y - self.current_y_loop
+        x = self.get_line_x_from_index(ti_x)
+        y = self.get_line_y_from_index(ti_y)
+        return x, y
+
+    def update_vertical_lines(self):
+        start_index = -int(self.V_NB_LINES / 2) + 1
+
+        for i in range(start_index, start_index + self.V_NB_LINES):
+            line_x = self.get_line_x_from_index(i)
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
+
+    def update_horizontal_lines(self):
+        start_index = -int(self.V_NB_LINES / 2) + 1
+        end_index = start_index + self.V_NB_LINES - 1
+
+        xmin = self.get_line_x_from_index(start_index)
+        xmax = self.get_line_x_from_index(end_index)
+
+        for i, line in enumerate(self.horizontal_lines):
+            line_y = self.get_line_y_from_index(i)
+            x1, y1 = self.transform(xmin, line_y)
+            x2, y2 = self.transform(xmax, line_y)
+            line.points = [x1, y1, x2, y2]
+
+    def update_tiles(self):
+        for i in range(0, self.NB_TILES):
+            tile = self.tiles[i]
+            tile_coordinates = self.tiles_coordinates[i]
+            xmin, ymin = self.get_tile_coordinates(tile_coordinates[0], tile_coordinates[1])
+            xmax, ymax = self.get_tile_coordinates(tile_coordinates[0] + 1, tile_coordinates[1] + 1)
+
+            x1, y1 = self.transform(xmin, ymin)
+            x2, y2 = self.transform(xmin, ymax)
+            x3, y3 = self.transform(xmax, ymax)
+            x4, y4 = self.transform(xmax, ymin)
+
+            tile.points = [x1, y1, x2, y2, x3, y3, x4, y4]
+
+    def update(self, dt):
+        time_factor = dt * 60
+        self.update_vertical_lines()
+        self.update_horizontal_lines()
+        self.update_tiles()
+        self.update_ship()
+
+        speed_y = self.SPEED * self.height / 100
+        self.current_offset_y += speed_y * time_factor
+        spacing_y = self.H_LINES_SPACING * self.height
+        if self.current_offset_y >= spacing_y:
+            self.current_offset_y -= spacing_y
+            self.current_y_loop += 1
+            self.generate_tiles_coordinates()
+
+        speed_x = (self.current_speed_x * self.width) / 100
+        self.current_offset_x += self.current_speed_x * time_factor
+
+class GalaxyApp(App):
+    pass
+
+
+GalaxyApp().run()
